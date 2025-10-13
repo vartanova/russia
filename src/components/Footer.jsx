f181654f766632a3974d3d0b5f8a2b6a6a82ee3f
vartanova <vartanova.fortech@gmail.com>
2025-10-13 18:16:59 +0300

    change tag div on nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex justify-between max-[768px]:flex-col">
-      <div className="flex gap-5 max-[768px]:flex-col max-[768px]:text-2xl max-[768px]:mb-12">
+      <nav className="flex gap-5 max-[768px]:flex-col max-[768px]:text-2xl max-[768px]:mb-12">
         <a href="https://yandex.ru/maps">Карты</a>
         <a href="https://yandex.ru/pogoda">Погода</a>
         <a href="https://rasp.yandex.ru">Расписание</a>
         <a href="https://calendar.yandex.ru">Каледарь</a>
         <a href="https://travel.yandex.ru">Путешествия</a>
-      </div>
+      </nav>
       <div className="text-[14px]">© 2020. Впишите своё имя</div>
     </div>
   );
