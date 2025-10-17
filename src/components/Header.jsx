5678255ebb576872147f43d97cc8983fe2847d24
vartanova <vartanova.fortech@gmail.com>
2025-10-17 13:39:28 +0300

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 import SwitchLocale from "./SwitchLocale";
 
 const Header = () => {
-
   const { t } = useTranslation("header");
 
   return (
@@ -14,12 +13,7 @@ const Header = () => {
         <a href="#">
           <img className="max-[400px]:max-w-36" src={logo} alt="logo" />
         </a>
-        <Link
-          to="/"
-          className="pt-3.5 cursor-pointer focus:underline underline-offset-3 decoration-1 under text-white text-2xl max-[607px]:text-lg max-[400px]:hidden"
-        >
-          {t("nav.home")}
-        </Link>
+
         <Link
           to="/travel"
           className="pt-3.5 cursor-pointer focus:underline underline-offset-3 decoration-1 under text-white text-2xl max-[607px]:text-lg max-[400px]:hidden"
@@ -29,7 +23,7 @@ const Header = () => {
       </nav>
 
       <div className="flex gap-4">
-        <SwitchLocale/>
+        <SwitchLocale />
       </div>
     </div>
   );
