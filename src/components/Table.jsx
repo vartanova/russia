45cc0a10bf9cd51f567d9e5672c93c89c57d8668
vartanova <vartanova.fortech@gmail.com>
2025-10-25 14:58:44 +0300

    adaptive

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -23,11 +23,11 @@ const Table = () => {
   return (
     <div className="w-full  rounded-sm">
       <table className="w-full">
-        <thead className="h-14">
+        <thead className="h-14 max-[450px]:h-10">
           {tableInstance.getHeaderGroups().map((headerGroups) => (
             <tr key={headerGroups.id}>
               {headerGroups.headers.map((header) => (
-                <th key={header.id} className="text-start pl-2.5">
+                <th key={header.id} className="text-start pl-2.5 max-[884px]:text-xs max-[450px]:text-[8px] max-[450px]:max-w-1/2">
                   {flexRender(
                     header.column.columnDef.header,
                     header.getContext()
@@ -41,7 +41,7 @@ const Table = () => {
           {tableInstance.getRowModel().rows.map((row) => (
             <tr key={row.id} className="hover:bg-[#4e6813] hover:cursor-pointer">
               {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} className="p-2.5 border-t">
+                <td key={cell.id} className="p-2.5 border-t max-[884px]:text-xs max-[450px]:text-[8px] max-[450px]:p-2">
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               ))}
--- src/pages/travel/TravelRequestPage.jsx
+++ src/pages/travel/TravelRequestPage.jsx
@@ -10,26 +10,26 @@ const TravelRequestPage = ({ travel__title = "travel__title", pt }) => {
   const { t } = useTranslation("travelrequestpage");
 
   return (
-    <div className="travel__requests min-h-screen px-14 py-10 pb-20 flex flex-col gap-10 ">
+    <div className="travel__requests min-h-screen px-14 py-10 pb-20 flex flex-col gap-10 max-[500px]:px-7 max-[500px]:py-7">
       <div className="flex items-start">
         <div className="flex justify-between w-full max-sm:mb-5">
-          <div>
+          <div className="max-[884px]:w-[300px]">
             <h1
-              className={`relative flex items-start font-bold leading-27 ${travel__title} text-9xl mb-4 w-[700px]`}
+              className={`relative flex items-start font-bold leading-27 ${travel__title} text-9xl mb-4 w-[700px] max-[884px]:text-7xl max-[884px]:leading-16 max-[500px]:text-2xl max-[500px]:mb-0`}
             >
-              <div className="absolute top-1/7 right-1/45 flex items-center justify-center cursor-pointer h-1/3 bg-[#4e6813] rounded-lg transition-all duration-500 ease-in-out w-6 hover:w-35">
+              <div className="absolute top-1/7 right-1/45 flex items-center justify-center cursor-pointer h-1/3 bg-[#4e6813] rounded-lg transition-all duration-500 ease-in-out w-6 hover:w-35 max-[884px]:top-4/7 max-[884px]:right-75 max-[500px]:top-[126px] max-[500px]:left-[80px] max-[500px]:w-40">
                 <BtnBack />
               </div>
               {t("title")}
             </h1>
-            <p className="w-1/2 travel__title">{t("subtitle")}</p>
+            <p className="w-1/2 travel__title max-[884px]:w-1/1 max-[500px]:text-xs max-[500px]:w-2/3">{t("subtitle")}</p>
           </div>
         </div>
-        <div className={`${travel__title}`}>
+        <div className={`h-[170px] flex items-start max-[604px]:items-end max-[500px]:h-[80px] max-[500px]:absolute max-[500px]:top-[100px]`}>
           <SwitchLocale travel__title={`${travel__title}`} pt={`${pt}`} />
         </div>
       </div>
-      <div className="flex flex-col gap-2 mx-40 px-9 py-9 border rounded-4xl backdrop-blur-lg bg-[#52525233]">
+      <div className="flex flex-col gap-2 mx-40 p-9 border rounded-4xl backdrop-blur-lg bg-[#52525233] max-[884px]:mx-0 max-[450px]:p-4 max-[450px]:rounded-2xl">
         <div className="w-full flex items-center rounded-lg bg-[#ffffff20]">
           <div className="pl-2.5">
             <PiMagnifyingGlassThin />
@@ -37,7 +37,7 @@ const TravelRequestPage = ({ travel__title = "travel__title", pt }) => {
           <input
             type="text"
             placeholder={t("placeholder")}
-            className=" w-full h-10 outline-none px-2.5 placeholder:text-white placeholder:text-xs"
+            className=" w-full h-10 outline-none px-2.5 placeholder:text-white placeholder:text-xs max-[450px]:text-[8px] max-[450px]:placeholder:text-[8px] max-[450px]:h-6"
           />
         </div>
         <Table />
